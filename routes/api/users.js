a5fe9650ebf2d4b61e8759ea7b50e9288b85f7bb
Vanessa Henderson <bookvanny@gmail.com>
2019-04-12 19:53:21 -0400

    register users functionality added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+const User = require('../../models/User');
 
 // tests if users route works, access is public
 router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
+
+// registers the user, access is public 
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if(user) {
+        return res.status(400).json({email: 'That email is already taken.'});
+      } else {
+        const avatar = gravatar.url(req.body.email, {
+          s: '200', // size
+          r: 'pg', // rating
+          d: 'mm' // default
+        });
+
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar: avatar, // with es6 you could just put avatar once here and take out semi-colon 
+          password: req.body.password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if(err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          })
+        })
+      }
+    })
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -7,6 +8,10 @@ const posts = require('./routes/api/posts');
 
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 const db = require('./config/keys').mongoURI;
 
 
